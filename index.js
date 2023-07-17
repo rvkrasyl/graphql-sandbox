6d19bb98e1cb0672c4d20c983ef54f3488ed02ff
rvkrasyl <rvkrasyl@gmail.com>
2023-07-17 18:03:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("GraphQL is amazing!");
+});
+
+const root = {
+  hello: () => "Hi from GraphQL",
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(5224, () => console.log("Server running in port localhost:5224/"));
